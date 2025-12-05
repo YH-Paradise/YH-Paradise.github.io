76b7a9357a32a54a72ce4d573cfabb3164875a8f
YH-Paradise <goodtobehomeyh@gmail.com>
2025-12-05 13:43:04 +0000

    Deploying to gh-pages from @ YH-Paradise/YH-Paradise.github.io@f2655909d9def447b642c4711a725c7b7d12b218 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -377,16 +377,19 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-started-my-bachelor-degree-dual-majoring-health-amp-amp-environmental-science-and-artificial-intelligence-at-korea-university-republic-of-korea",
+          title: 'I started my Bachelor degree dual majoring Health &amp;amp;amp; Environmental Science and Artificial...',
+          description: "",
+          section: "News",},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
